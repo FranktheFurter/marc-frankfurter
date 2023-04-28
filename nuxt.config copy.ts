419235bd8c74c47279f4b068850312e08a7bb023
Marc Frankfurter <Marc.Frankfurter@scheppach.com>
2023-04-28 13:23:44 +0200

    first commit

---
+++ nuxt.config copy.ts
@@ -0,0 +1,9 @@
+// nuxt.config.ts
+//last try
+export default defineNuxtConfig({
+  modules: ["@unocss/nuxt"],
+  app: {
+    baseURL: "/nuxt-sandbox/",
+  },
+  ssr: false,
+})
---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// nuxt.config.ts
+//last try
+export default defineNuxtConfig({
+  modules: ["@unocss/nuxt"],
+  app: {
+    baseURL: "/marc-frankfurter/",
+  },
+  ssr: false,
+})
